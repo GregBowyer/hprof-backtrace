aa17b58041ba6bf6899249b8146baac75b7fe07f
Greg Bowyer <gbowyer@fastmail.co.uk>
2012-03-30 22:23:22 -0700

    GCC Aligns structs we will have to read differently

--- hprof_backtrace.c
+++ hprof_backtrace.c
@@ -26,12 +26,18 @@ int main(int argc, char** argv) {
     die(hprof_fp == NULL, "File error");
 
     hprof_header* header;
-    int err = fread(header, sizeof(hprof_header), 1, hprof_fp);
-    printf("%d\n", err);
+    char version[19];
+    u4 ident_size;
 
-    printf("Header: version=%s identsize=%ju \n", 
-            header->version,
-            (uintmax_t) be32_to_native(header->ident_size));
+    int count = fread(version, sizeof(version), 1, hprof_fp);
+    die(count != 1, "Unable to read hprof header");
+
+    count = fread(&ident_size, sizeof(u4), 1, hprof_fp);
+    die(count != 1, "Unable to read hprof header");
+
+    printf("Header: version=%s identsize=%ju\n",
+            version,
+            (uintmax_t) be32_to_native(ident_size));
 
     fclose(hprof_fp);
     return 0;
