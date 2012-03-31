417d1c82abbb52e2e894532bdc137ca106fc15c6
Greg Bowyer <gbowyer@fastmail.co.uk>
2012-03-30 21:35:04 -0700

    Moved hprof defns to header

---
+++ hprof_backtrace.h
@@ -0,0 +1,72 @@
+#ifndef __hprof_backtrace_h__
+#define __hprof_backtrace_h__
+
+#include <stdint.h>
+
+// This is what the documentation refers to
+// *Endieness unknown, assume native*
+typedef uint8_t u1;
+typedef uint32_t u4;
+
+typedef enum {
+  // top-level records
+  HPROF_UTF8                    = 0x01,
+  HPROF_LOAD_CLASS              = 0x02,
+  HPROF_UNLOAD_CLASS            = 0x03,
+  HPROF_FRAME                   = 0x04,
+  HPROF_TRACE                   = 0x05,
+  HPROF_ALLOC_SITES             = 0x06,
+  HPROF_HEAP_SUMMARY            = 0x07,
+  HPROF_START_THREAD            = 0x0A,
+  HPROF_END_THREAD              = 0x0B,
+  HPROF_HEAP_DUMP               = 0x0C,
+  HPROF_CPU_SAMPLES             = 0x0D,
+  HPROF_CONTROL_SETTINGS        = 0x0E,
+
+  // 1.0.2 record types
+  HPROF_HEAP_DUMP_SEGMENT       = 0x1C,
+  HPROF_HEAP_DUMP_END           = 0x2C,
+
+  // field types
+  HPROF_ARRAY_OBJECT            = 0x01,
+  HPROF_NORMAL_OBJECT           = 0x02,
+  HPROF_BOOLEAN                 = 0x04,
+  HPROF_CHAR                    = 0x05,
+  HPROF_FLOAT                   = 0x06,
+  HPROF_DOUBLE                  = 0x07,
+  HPROF_BYTE                    = 0x08,
+  HPROF_SHORT                   = 0x09,
+  HPROF_INT                     = 0x0A,
+  HPROF_LONG                    = 0x0B,
+
+  // data-dump sub-records
+  HPROF_GC_ROOT_UNKNOWN         = 0xFF,
+  HPROF_GC_ROOT_JNI_GLOBAL      = 0x01,
+  HPROF_GC_ROOT_JNI_LOCAL       = 0x02,
+  HPROF_GC_ROOT_JAVA_FRAME      = 0x03,
+  HPROF_GC_ROOT_NATIVE_STACK    = 0x04,
+  HPROF_GC_ROOT_STICKY_CLASS    = 0x05,
+  HPROF_GC_ROOT_THREAD_BLOCK    = 0x06,
+  HPROF_GC_ROOT_MONITOR_USED    = 0x07,
+  HPROF_GC_ROOT_THREAD_OBJ      = 0x08,
+  HPROF_GC_CLASS_DUMP           = 0x20,
+  HPROF_GC_INSTANCE_DUMP        = 0x21,
+  HPROF_GC_OBJ_ARRAY_DUMP       = 0x22,
+  HPROF_GC_PRIM_ARRAY_DUMP      = 0x23
+} hprofTag;
+
+typedef struct {
+    char version[19];
+    u4 ident_size;
+    u4 high_word;
+    u4 low_word;
+} hprof_header;
+
+typedef struct {
+    u1 tag;
+    u4 profiling_ms;
+    u4 record_remaining_bytes;
+    //[u1]* body of record
+} hprof_record_header;
+
+#endif
