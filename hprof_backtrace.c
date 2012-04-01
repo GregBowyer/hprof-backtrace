bb24ef70acb7f55463e1f06c64b8358a2696717b
Greg Bowyer <gbowyer@fastmail.co.uk>
2012-03-31 19:50:26 -0700

    Initial dumb code to read through the hprof records

--- hprof_backtrace.c
+++ hprof_backtrace.c
@@ -10,9 +10,16 @@
 
 void die(int check, char* message) {
     if (check) {
-        printf(message);
+        printf("%s\n", message);
         exit(1);
     }
+}
+
+int read_record_header(hprof_record_header* record_header, FILE* hprof_fp) {
+    int count = fread(&record_header->tag, sizeof(u1), 1, hprof_fp);
+    count += fread(&record_header->profiling_ms, sizeof(u4), 1, hprof_fp);
+    count += fread(&record_header->remaining_bytes, sizeof(u4), 1, hprof_fp);
+    return !(count == 3);
 }
 
 int main(int argc, char** argv) {
@@ -35,9 +42,57 @@ int main(int argc, char** argv) {
     count = fread(&ident_size, sizeof(u4), 1, hprof_fp);
     die(count != 1, "Unable to read hprof header");
 
+    int success = fseek(hprof_fp, sizeof(u8), SEEK_CUR);
+    die(success != 0, "Could not seek forward in the file");
+
     printf("Header: version=%s identsize=%ju\n",
             version,
             (uintmax_t) be32_to_native(ident_size));
+
+    // For now just reuse it
+    hprof_record_header* record_header = calloc(1, sizeof(hprof_record_header));
+
+    while(1) {
+        if (feof(hprof_fp)) {
+            break;
+        }
+
+        read_record_header(record_header, hprof_fp);
+
+        switch(record_header->tag) {
+            case UTF8:
+                printf("UTF8\n");
+            case LOAD_CLASS:
+                printf("LOAD_CLASS\n");
+            case UNLOAD_CLASS:
+                printf("UNLOAD_CLASS\n");
+            case FRAME:
+                printf("FRAME\n");
+            case TRACE:
+                printf("TRACE\n");
+            case ALLOC_SITES:
+                printf("ALLOC_SITES\n");
+            case HEAP_SUMMARY:
+                printf("HEAP_SUMMARY\n");
+            case START_THREAD:
+                printf("START_THREAD\n");
+            case END_THREAD:
+                printf("END_THREAD\n");
+            case HEAP_DUMP:
+                printf("HEAP_DUMP\n");
+            case CPU_SAMPLES:
+                printf("CPU_SAMPLES\n");
+            case CONTROL_SETTINGS:
+                printf("CONTROL_SETTINGS\n");
+            case HEAP_DUMP_SEGMENT:
+                printf("HEAP_DUMP_SEGMENT\n");
+            case HEAP_DUMP_END:
+                printf("HEAP_DUMP_END\n");
+            default:
+                fseek(hprof_fp, record_header->remaining_bytes, SEEK_CUR);
+                break;
+        }
+    }
 
     fclose(hprof_fp);
     return 0;
--- hprof_backtrace.h
+++ hprof_backtrace.h
@@ -7,65 +7,64 @@
 // *Endieness unknown, assume native*
 typedef uint8_t u1;
 typedef uint32_t u4;
+typedef uint64_t u8;
 
 typedef enum {
-  // top-level records
-  HPROF_UTF8                    = 0x01,
-  HPROF_LOAD_CLASS              = 0x02,
-  HPROF_UNLOAD_CLASS            = 0x03,
-  HPROF_FRAME                   = 0x04,
-  HPROF_TRACE                   = 0x05,
-  HPROF_ALLOC_SITES             = 0x06,
-  HPROF_HEAP_SUMMARY            = 0x07,
-  HPROF_START_THREAD            = 0x0A,
-  HPROF_END_THREAD              = 0x0B,
-  HPROF_HEAP_DUMP               = 0x0C,
-  HPROF_CPU_SAMPLES             = 0x0D,
-  HPROF_CONTROL_SETTINGS        = 0x0E,
+    UTF8                    = 0x01,
+    LOAD_CLASS              = 0x02,
+    UNLOAD_CLASS            = 0x03,
+    FRAME                   = 0x04,
+    TRACE                   = 0x05,
+    ALLOC_SITES             = 0x06,
+    HEAP_SUMMARY            = 0x07,
+    START_THREAD            = 0x0A,
+    END_THREAD              = 0x0B,
+    HEAP_DUMP               = 0x0C,
+    HEAP_DUMP_SEGMENT       = 0x1C, /* 1.0.2 only */
+    HEAP_DUMP_END           = 0x2C, /* 1.0.2 only */
+    CPU_SAMPLES             = 0x0D,
+    CONTROL_SETTINGS        = 0x0E
+} hprof_tag;
 
-  // 1.0.2 record types
-  HPROF_HEAP_DUMP_SEGMENT       = 0x1C,
-  HPROF_HEAP_DUMP_END           = 0x2C,
+typedef enum {
+    GC_ROOT_UNKNOWN       = 0xFF,
+    GC_ROOT_JNI_GLOBAL    = 0x01,
+    GC_ROOT_JNI_LOCAL     = 0x02,
+    GC_ROOT_JAVA_FRAME    = 0x03,
+    GC_ROOT_NATIVE_STACK  = 0x04,
+    GC_ROOT_STICKY_CLASS  = 0x05,
+    GC_ROOT_THREAD_BLOCK  = 0x06,
+    GC_ROOT_MONITOR_USED  = 0x07,
+    GC_ROOT_THREAD_OBJ    = 0x08,
+    GC_CLASS_DUMP         = 0x20,
+    GC_INSTANCE_DUMP      = 0x21,
+    GC_OBJ_ARRAY_DUMP     = 0x22,
+    GC_PRIM_ARRAY_DUMP    = 0x23
+} hprof_gc_tag;
 
-  // field types
-  HPROF_ARRAY_OBJECT            = 0x01,
-  HPROF_NORMAL_OBJECT           = 0x02,
-  HPROF_BOOLEAN                 = 0x04,
-  HPROF_CHAR                    = 0x05,
-  HPROF_FLOAT                   = 0x06,
-  HPROF_DOUBLE                  = 0x07,
-  HPROF_BYTE                    = 0x08,
-  HPROF_SHORT                   = 0x09,
-  HPROF_INT                     = 0x0A,
-  HPROF_LONG                    = 0x0B,
-
-  // data-dump sub-records
-  HPROF_GC_ROOT_UNKNOWN         = 0xFF,
-  HPROF_GC_ROOT_JNI_GLOBAL      = 0x01,
-  HPROF_GC_ROOT_JNI_LOCAL       = 0x02,
-  HPROF_GC_ROOT_JAVA_FRAME      = 0x03,
-  HPROF_GC_ROOT_NATIVE_STACK    = 0x04,
-  HPROF_GC_ROOT_STICKY_CLASS    = 0x05,
-  HPROF_GC_ROOT_THREAD_BLOCK    = 0x06,
-  HPROF_GC_ROOT_MONITOR_USED    = 0x07,
-  HPROF_GC_ROOT_THREAD_OBJ      = 0x08,
-  HPROF_GC_CLASS_DUMP           = 0x20,
-  HPROF_GC_INSTANCE_DUMP        = 0x21,
-  HPROF_GC_OBJ_ARRAY_DUMP       = 0x22,
-  HPROF_GC_PRIM_ARRAY_DUMP      = 0x23
-} hprofTag;
+typedef enum {
+    ARRAY_OBJECT      = 1,
+    NORMAL_OBJECT     = 2,
+    BOOLEAN           = 4,
+    CHAR              = 5,
+    FLOAT             = 6,
+    DOUBLE            = 7,
+    BYTE              = 8,
+    SHORT             = 9,
+    INT               = 10,
+    LONG              = 11
+} hprof_object_type;
 
 typedef struct {
-    char version[19];
     u4 ident_size;
     u4 high_word;
     u4 low_word;
 } hprof_header;
 
 typedef struct {
-    u1 tag;
+    hprof_tag tag;
     u4 profiling_ms;
-    u4 record_remaining_bytes;
+    u4 remaining_bytes;
     //[u1]* body of record
 } hprof_record_header;
 
